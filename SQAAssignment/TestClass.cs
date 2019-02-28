3efa58f8e5cb0991b413c5c6d80b43af4cfdf64e
fdavey22 <S00170902@mail.itsligo.ie>
2019-02-28 13:09:38 +0000

    Test Class made

---
+++ SQAAssignment/TestClass.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SQAAssignment
+{
+    [TestFixture]
+    public class TestClass
+    {
+    }
+}
