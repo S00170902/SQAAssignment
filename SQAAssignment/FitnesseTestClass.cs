709f370f2996f2d40343458997b382eb251a630e
fdavey22 <S00170902@mail.itsligo.ie>
2019-02-28 15:30:11 +0000

    Fitnesse Test Fixture written

---
+++ SQAAssignment/FitnesseTestClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using fit;
+using fit.Fixtures;
+
+namespace SQAAssignment
+{
+    public class FitnesseTestClass: ColumnFixture
+    {
+        public string gender;
+        public int age;
+
+        public PremiumSheet ps = new PremiumSheet();
+
+        public float premium()
+        {
+            return ps.premium();
+        }
+    }
+
+    public class PremiumSheet
+    {
+        private string gender;
+        private int age;
+
+        public void setproperties(string g, int a)
+        {
+            gender = g;
+            age = a;
+        }
+
+        public float premium()
+        {
+            InsuranceService insuranceService = new InsuranceService();
+            return insuranceService.CalcPremium(age, gender);
+        }
+    }
+}
