46bf14fa67ff45db1853b38da0c2a8ac9ef3fe2e
David Fox <S00172018@mail.itsligo.ie>
2019-03-02 20:13:25 +0000

    More White Box Tests (Age > 50 still to be done)

--- SQAAssignment/TestClass.cs
+++ SQAAssignment/TestClass.cs
@@ -26,5 +26,49 @@ namespace SQAAssignment
 
             Assert.AreEqual(expectedResult, actualResult);
         }
+        [Test]
+        public static void female_35_2_5()
+        {
+            expectedResult = 2.50f;
+            float actualResult = insuranceService.CalcPremium(35, "female");
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public static void female_17_0()
+        {
+            expectedResult = 0f;
+            float actualResult = insuranceService.CalcPremium(17, "female");
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public static void male_30_6()
+        {
+            expectedResult = 6.0f;
+            float actualResult = insuranceService.CalcPremium(30, "male");
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        public static void male_40_5()
+        {
+            expectedResult = 5.0f;
+            float actualResult = insuranceService.CalcPremium(40, "male");
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        public static void male_17_0()
+        {
+            expectedResult = 0f;
+            float actualResult = insuranceService.CalcPremium(17, "male");
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+      
     }
 }
