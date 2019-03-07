d77707562a2fecf4977c8a8b6f455d46181dcf89
fdavey22 <S00170902@mail.itsligo.ie>
2019-03-07 12:00:23 +0000

    UI implemented

---
+++ SQAAssignment/WebForm1.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="SQAAssignment.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="age" runat="server"></asp:TextBox>
+            <asp:RadioButton ID="male" runat="server" GroupName="gender" Text="Male" OnCheckedChanged="rb_CheckedChanged" />
+            <asp:RadioButton ID="female" runat="server" GroupName="gender" Text="Female" OnCheckedChanged="rb_CheckedChanged" />
+            <asp:Button ID="submit" runat="server" Text="Submit" OnClick="submit_Click" />
+            <asp:Label ID="prem" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ SQAAssignment/WebForm1.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SQAAssignment
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        static int ageInput;
+        static string genderInput;
+        static InsuranceService ins = new InsuranceService();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void rb_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton g = sender as RadioButton;
+            genderInput = g.Text;
+        }
+
+        protected void submit_Click(object sender, EventArgs e)
+        {
+            string a = age.Text;
+            int ageInput = int.Parse(a);
+
+            float premium = ins.CalcPremium(ageInput, genderInput);
+
+            prem.Text = "Your premium is: " + premium.ToString();
+        }
+    }
+}
